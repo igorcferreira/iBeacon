6059088466dc2d9393dcb21ba2bc5f41d74aabd7
Igor Ferreira <igorcferreira@hotmail.com>
2015-07-28 13:34:49 -0300

    Initial Commit

---
+++ BeaconTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconTutorial
+//
+//  Created by Igor Ferreira on 7/28/15.
+//  Copyright © 2015 POGAmadores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconTutorial
+//
+//  Created by Igor Ferreira on 7/28/15.
+//  Copyright © 2015 POGAmadores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
